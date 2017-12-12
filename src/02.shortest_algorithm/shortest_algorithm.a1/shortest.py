1554f6f4e77bf2f24eaa37bbe6f3598cc05009b1
mo-mosverkstad <34004324+mo-mosverkstad@users.noreply.github.com>
2017-12-12 21:12:13 +0100

    shortest_algorithm: small fix
    Fix the small bug to re-calculate the next nodes.

--- src/02.shortest_algorithm/shortest_algorithm.a1/shortest.py
+++ src/02.shortest_algorithm/shortest_algorithm.a1/shortest.py
@@ -7,7 +7,6 @@ def lookfor_shortest(si, ei, skynet_map):
     visiting_nodes = {si}
     visited_nodes = set()
     while visiting_nodes:
-        next_nodes = set()
         for sn in visiting_nodes:
             for dn in range(len(skynet_map)):
                 #print 'si=%d, sn=%d, dn=%d'%(si, sn, dn)
@@ -16,10 +15,10 @@ def lookfor_shortest(si, ei, skynet_map):
                    and ([sn][0] + skynet_map[sn][dn] < MAX_VALUE) \
                    and sn != dn:
                     weight[dn] = (weight[sn][0] + skynet_map[sn][dn], sn)
-                    next_nodes.add(dn)
-        #print '== weight ', weight
-        #print '== next   ', next_nodes
         visited_nodes.update(visiting_nodes)
+        #print '== weight ', weight
+        next_nodes = {i for i in range(len(weight)) if weight[i][0] != MAX_VALUE}
+        #print '== next   ', next_nodes
         visiting_nodes = next_nodes - visited_nodes
         #print '== visited', visited_nodes
         #print '== visitin', visiting_nodes
