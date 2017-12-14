c286d276964229fac7fd35a77cb7bb3be2842f6a
mo-mosverkstad <34004324+mo-mosverkstad@users.noreply.github.com>
2017-12-14 15:04:36 +0100

    shortest_algorithm: b1, new test platform
    New test platform with two different types of graph.

--- src/02.shortest_algorithm/shortest_algorithm.b1/test.py
+++ src/02.shortest_algorithm/shortest_algorithm.b1/test.py
@@ -2,15 +2,28 @@
 
 from graph import Graph
 
-graph_data = {'skynet01': ([0,1,2,3],[(0,1),(0,2),(1,3),(2,3)])}
+graph_data = {'skynet01': ([0,1,2,3],[(0,1),(0,2),(1,3),(2,3)],[(0,3,2)]), \
+              'skynet02': ([1,2,3,4,5,6], [(1,2,7),(1,3,9),(1,6,14),(2,3,10),(2,4,15),(3,4,11),(3,6,2),(4,5,6),(6,5,9)],[(1,5,20)])}
 
 def test_graph(graph_item):
-    vertices, edges = graph_item
+    vertices, edges, path = graph_data[graph_item]
     graph = Graph()
     for v in vertices: graph.add_vertex(v)
-    for e in edges: graph.add_edge(e[0], e[1])
-    print graph
-    print graph._lookfor_shortest(0)
+    for e in edges: graph.add_edge(*e)
+    #print graph
+    result = True
+    for p in path:
+        si, ei, exp = p
+        w, p = graph.get_shortest_path(si,ei)
+        if w != exp:
+            result = False
+            print 'Fail: %s: %d->%d, expect %d but get %d!'%(graph_item, si, ei, exp, w)
+    return result
 
+def test_graphs(graph_data):
+    result = True
+    for g in graph_data.keys():
+        result = result and test_graph(g)
+    print '==== Test result: %s ===='%(str(result))
 
-test_graph(graph_data['skynet01'])
+test_graphs(graph_data)
